b313fb83ee00800b082b5a1541bf0be474626ead
Daniel Nagornov <danieln056@gmail.com>
2023-06-11 15:22:13 +0300

    infrastructure

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ var app = express();
 
 const port = process.env.PORT || 8080;
 
-const views_dir = path.join(__dirname, '/src/views/');
+const views_dir = path.join(__dirname, '/frontend/views/');
 
+// serve static files
+app.use(express.static(path.join(__dirname, "/frontend")));
+
+// routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(views_dir, "index.html"));
 });
