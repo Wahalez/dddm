92e247071a456bf5d1a5a02fa5cf5adbe62a8f2e
Daniel Nagornov <danieln056@gmail.com>
2023-06-11 18:13:09 +0300

    seperated routes logic from server.js

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router()
 
-const views_dir = path.join(__dirname, '/frontend/views/');
+const views_dir = path.join(__dirname, '../../frontend/views/');
 
 router.get("/", (req, res) => {
     res.sendFile(views_dir + "index.html");
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require('path');
 var app = express();
 
 const port = process.env.PORT || 8080;
-const routes_path = path.join(__dirname, "/backend/routes");
+const routes_path = path.join(__dirname, "/backend/routes/");
 
 // serve static files
 app.use(express.static(path.join(__dirname, "/frontend")));
 
 
 // routes
-const mainRoute = require(routes_path);
+const mainRoute = require(routes_path + "main");
 app.use("/", mainRoute);
 
 app.listen(port);
