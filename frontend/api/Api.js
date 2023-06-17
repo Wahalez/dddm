da8b1db18f1a16a8298962bc5bfcc5be04eba80c
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2023-06-17 17:10:06 +0300

    updated api and prettier code

--- frontend/api/Api.js
+++ frontend/api/Api.js
@@ -1,28 +1,25 @@
-$(function() {
-  // Set up Autocomplete
-  $("#city").autocomplete({
-    source: function(request, response) {
-      $.ajax({
-        url: "http://api.geonames.org/searchJSON",
-        dataType: "json",
-        data: {
-          country: "IL",
-          featureCode: "PPL",
-          maxRows: 1000,
-          username: "dddm",
-          name_startsWith: request.term
-        },
-        success: function(data) {
-          response(data.geonames.map(function(city) {
-            return city.name;
-          }));
-        },
-        error: function(xhr, status, error) {
-          console.error('Error:', error);
-          response([]);
-        }
-      });
-    },
-    minLength: 1 // Minimum number of characters before triggering autocomplete
-  });
+$("#city").autocomplete({
+  source: function (request, response) {
+    $.ajax({
+      url: "https://data.gov.il/api/3/action/datastore_search",
+      dataType: "json",
+      data: {
+        resource_id: "d4901968-dad3-4845-a9b0-a57d027f11ab",
+        limit: 10000,
+        q: request.term, // Pass the user's input as a query parameter
+      },
+      success: function (data) {
+        const records = data.result.records;
+        const cityNames = records.map((record) => record["שם_ישוב_לועזי"]);
+        cityArray = cityNames; // Assign the city names to the cityArray variable in cities.js
+        response(cityNames);
+      },
+      error: function (xhr, status, error) {
+        console.error("Error:", error);
+        response([]); // Return an empty array in case of error
+      },
+    });
+  },
+  minLength: 2, // Minimum number of characters before triggering autocomplete
+  delay: 300, // Delay in milliseconds before sending the AJAX request
 });
--- frontend/controller/index.js
+++ frontend/controller/index.js
@@ -1,69 +1,70 @@
 function refreshPage() {
-    window.location.reload();
+  window.location.reload();
 }
-document.getElementById('loginBtn').addEventListener('click', function() {
-  window.location.href = './../view/login.html';
+document.getElementById("loginBtn").addEventListener("click", function () {
+  window.location.href = "./../view/login.html";
 });
- document.getElementById('homebtn').addEventListener('click', function() {
-    window.location.href = './../view/index.html';
- });
- document.getElementById('registerbtn').addEventListener('click', function() {
-    window.location.href = './../view/Register.html';
+document.getElementById("homeBtn").addEventListener("click", function () {
+  window.location.href = "./../view/index.html";
 });
- document.getElementById('console').addEventListener('click', function() {
-    window.location.href = './../view/console.html';
+document.getElementById("registerBtn").addEventListener("click", function () {
+  window.location.href = "./../view/register.html";
 });
-  document.getElementById('allGames').addEventListener('click', function() {
-    window.location.href = './../view/games.html';
+document.getElementById("consoleBtn").addEventListener("click", function () {
+  window.location.href = "./../view/console.html";
+});
+document.getElementById("allgamesBtn").addEventListener("click", function () {
+  window.location.href = "./../view/games.html";
+});
+
+function comparePasswords(event) {
+  event.preventDefault(); // Prevent form submission
+
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  if (password === confirmPassword) {
+    // Passwords match, proceed with registration logic
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+
+    // Add your registration code here using the firstName and lastName variables
+
+    alert("Registration successful!");
+    window.location.href = "success.html";
+  }
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+  }
+
+  // Passwords match, proceed with registration
+  // Add your registration logic here
+  console.log("Registration successful");
+}
+
+$(function () {
+  var availableTags = [
+    "Sony playstations",
+    "Xbox",
+    "Nintendo",
+    "Games",
+    "Xbox games",
+    "Nintendo games",
+    "Sony playstations games",
+    "computer games",
+    "Console",
+  ];
+  $("#search").autocomplete({
+    source: availableTags,
   });
-  
-  function comparePasswords(event) {
-    event.preventDefault(); // Prevent form submission
+});
+function handleSearch(event) {
+  event.preventDefault(); // Prevent form submission
 
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
-    if (password === confirmPassword) {
-      // Passwords match, proceed with registration logic
-      var firstName = document.getElementById('firstName').value;
-      var lastName = document.getElementById('lastName').value;
+  // Get the search query from the input field
+  console.log("Search query:", searchQuery);
 
-      // Add your registration code here using the firstName and lastName variables
-
-      alert('Registration successful!');
-      window.location.href = 'success.html'
-    }
-    if (password !== confirmPassword) {
-      alert("Passwords do not match!");
-    }
-  
-    // Passwords match, proceed with registration
-    // Add your registration logic here
-    console.log("Registration successful");
-} 
-
-$(function() {
-    var availableTags = [
-      "Sony playstations",
-      "Xbox",
-      "Nintendo",
-      "Games",
-      "Xbox games",
-      "Nintendo games",
-      "Sony playstations games",
-      "computer games",
-      "Console",
-    ];
-    $("#search").autocomplete({
-      source: availableTags
-    });
-  });
-  function handleSearch(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get the search query from the input field
-    console.log("Search query:", searchQuery);
-
-    window.location.href = "search-results.html?q=" + encodeURIComponent(searchQuery);
+  window.location.href =
+    "search-results.html?q=" + encodeURIComponent(searchQuery);
 }
 var searchnameInput = document.getElementById("search");
 var searchnameValue = searchnameInput.value;
