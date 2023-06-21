8829edd4b322d3b55fde5af739fb5c52e1d0afd3
Master Chief <46121368+MasterChiefProject@users.noreply.github.com>
2023-06-21 11:35:58 +0300

    updated controller for header.js

--- frontend/controller/header.js
+++ frontend/controller/header.js
@@ -1,11 +1,30 @@
-$(document).ready(function () {
+const routes = () => {
+  $("#loginBtn").click(function () {
+    window.location.href = "/login";
+  });
+  $("#homeBtn").click(function () {
+    window.location.href = "/";
+  });
+  $("#registerBtn").click(function () {
+    window.location.href = "/register";
+  });
+  $("#console").click(function () {
+    window.location.href = "/consoles";
+  });
+  $("#allGames").click(function () {
+    window.location.href = "/games";
+  });
+};
+
+$(document).ready(() => {
   const header = $("#header");
-  if (header.length) {
+  if (header) {
     $.ajax({
       url: "header.html",
       dataType: "html",
       success: function (content) {
         header.html(content);
+        routes();
       },
       error: function (error) {
         console.error("Error loading header:", error);
